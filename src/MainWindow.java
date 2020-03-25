1e22a20f57f1601cdacae1ce1e1059406c7edefc
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2020-03-26 00:03:54 +0100

    Add files via upload

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -2,6 +2,7 @@
 import maps.Coordinate;
 import maps.Street;
 import maps.Stop;
+import maps.TransportLine;
 
 
 import javafx.scene.image.Image;
@@ -17,7 +18,9 @@ import javafx.scene.input.*;
 import java.awt.MouseInfo;
 import javafx.event.*;
 import java.util.*;
-import javafx.scene.Group;
+import java.util.stream.Collectors;
+
+import javafx.scene.shape.Circle;
 
 
 
@@ -30,7 +33,7 @@ public class MainWindow extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         ArrayList<Street> streets_list = new ArrayList<Street>();
-
+        ArrayList<Stop> stops_list = new ArrayList<Stop>();
 
         Pane root = new Pane();
 
@@ -39,28 +42,11 @@ public class MainWindow extends Application {
         Line line1 = null;
         Line line2 = null;
 
-        /*
-        Line line = new Line(540, 30,   540,   158);
-        line.setStroke(Color.VIOLET);
-        line.setStrokeWidth(5);
-        root.getChildren().addAll(line);
-
-         */
-        /*
-        Line line = new Line(540,30,540,158);
-        line.setStroke(Color.VIOLET);
-        line.setStrokeWidth(5);
-        root.getChildren().addAll(line);
-
-         */
-
         File file = new File("C:/Users/forto/IdeaProjects/proj/lib/map.png");
         BackgroundImage myBI= new BackgroundImage(new Image(file.toURI().toString()),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         root.setBackground(new Background(myBI));
 
-        //stage.setScene(scene);
-        //stage.show();
 
         Point2D p = MouseInfo.getPointerInfo().getLocation();
 
@@ -76,18 +62,9 @@ public class MainWindow extends Application {
 
         for (Street s : streets_list)
         {
-            //System.out.println(s.getCoordinates());
             if (s.getCoordinates().get(1) != null)
             {
-                /*
-                System.out.println("Three coordinates street");
-                System.out.println(s.getId());
-                System.out.println(s.getCoordinates().get(0).getX());
-                System.out.println(s.getCoordinates().get(0).getY());
-                System.out.println(s.getCoordinates().get(1).getX());
-                System.out.println(s.getCoordinates().get(1).getY());
 
-                 */
                 line1 = new Line(s.getCoordinates().get(0).getX(), s.getCoordinates().get(0).getY(),   s.getCoordinates().get(1).getX(),   s.getCoordinates().get(1).getY());
                 line2 = new Line(s.getCoordinates().get(1).getX(), s.getCoordinates().get(1).getY(),   s.getCoordinates().get(2).getX(),   s.getCoordinates().get(2).getY());
                 line1.setStroke(Color.DARKRED);
@@ -109,7 +86,7 @@ public class MainWindow extends Application {
 
         }
 
-        setMapStops(streets_list);
+        stops_list = setMapStops(streets_list);
 
         for (Street s : streets_list)
         {
@@ -117,16 +94,61 @@ public class MainWindow extends Application {
             System.out.println("Follows/Not Followes");
             for (Street o : streets_list)
             {
-                //System.out.println(o.getId());
                 if (o.equals(s))
                 {
                     continue;
                 }
 
                 System.out.println(o.getId() + ": " + s.follows(o));
+            }
+        }
+
+        for (Stop stop : stops_list)
+        {
+            Circle circle = new Circle(stop.getCoordinate().getX(), stop.getCoordinate().getY(), 5);
+            circle.setStroke(Color.YELLOWGREEN);
+            circle.setStrokeWidth(5);
+            root.getChildren().addAll(circle);
+        }
 
 
+        TransportLine transportLine = setScheduleLines(streets_list, stops_list);
+        for (Stop stop : stops_list)
+        {
+            if (transportLine.getStopsMap().contains(stop))
+            {
+                Circle circle = new Circle(stop.getCoordinate().getX(), stop.getCoordinate().getY(), 5);
+                circle.setStroke(Color.PINK);
+                circle.setStrokeWidth(5);
+                root.getChildren().addAll(circle);
+            }
 
+        }
+
+        for (Street s : streets_list)
+        {
+            if (transportLine.getStreetsMap().contains(s))
+            {
+                if (s.getCoordinates().get(1) != null)
+                {
+
+                    line1 = new Line(s.getCoordinates().get(0).getX(), s.getCoordinates().get(0).getY(),   s.getCoordinates().get(1).getX(),   s.getCoordinates().get(1).getY());
+                    line2 = new Line(s.getCoordinates().get(1).getX(), s.getCoordinates().get(1).getY(),   s.getCoordinates().get(2).getX(),   s.getCoordinates().get(2).getY());
+                    line1.setStroke(Color.PINK);
+                    line1.setStrokeWidth(5);
+                    line2.setStroke(Color.PINK);
+                    line2.setStrokeWidth(5);
+                    root.getChildren().addAll(line1, line2);
+
+                }
+                else
+                {
+                    line1 = new Line(s.getCoordinates().get(0).getX(), s.getCoordinates().get(0).getY(),   s.getCoordinates().get(2).getX(),   s.getCoordinates().get(2).getY());
+                    line1.setStroke(Color.PINK);
+                    line1.setStrokeWidth(5);
+                    root.getChildren().addAll(line1);
+
+                }
             }
 
         }
@@ -152,46 +174,31 @@ public class MainWindow extends Application {
             int street_coordinates3_x = -1;
             int street_coordinates3_y = -1;
             boolean right_angle_street = false;
-            //System.out.println(line);
+
             String street_id = line.substring(0,(line.indexOf("-"))).trim();
             String street_coordinates1 = line.substring((line.indexOf("-") + 1),line.indexOf(";")).trim();
             String street_coordinates2 = line.substring((line.indexOf(";") + 1)).trim();
 
             if (street_coordinates2.contains(";"))
             {
-                //System.out.println("Three points for street");
                 street_coordinates3 = (street_coordinates2.substring((street_coordinates2.indexOf(";") + 1)).trim());
                 street_coordinates2 = (street_coordinates2.substring(0,(street_coordinates2.indexOf(";"))).trim());
-                //street_coordinates3 = (line.substring((street_coordinates2.indexOf(";") + 1)).trim());
                 right_angle_street = true;
 
 
             }
 
-            /*
-            System.out.println(street_id);
-            System.out.println(street_coordinates1);
-            System.out.println(street_coordinates2);
-            System.out.println(street_coordinates3);
-
-             */
 
             int street_coordinates1_x = Integer.parseInt(street_coordinates1.substring(1, (street_coordinates1.indexOf(","))));
-            //System.out.println(street_coordinates1_x);
             int street_coordinates1_y = Integer.parseInt(street_coordinates1.substring(street_coordinates1.indexOf(",")+1,street_coordinates1.length()-1));
-           // System.out.println(street_coordinates1_y);
 
             int street_coordinates2_x = Integer.parseInt(street_coordinates2.substring(1, (street_coordinates2.indexOf(","))));
-           // System.out.println(street_coordinates2_x);
             int street_coordinates2_y = Integer.parseInt(street_coordinates2.substring(street_coordinates2.indexOf(",")+1,street_coordinates2.length()-1));
-           // System.out.println(street_coordinates2_y);
 
             if (right_angle_street == true) // street with right angle - three init points
             {
                 street_coordinates3_x = Integer.parseInt(street_coordinates3.substring(1, (street_coordinates3.indexOf(","))));
-                //System.out.println(street_coordinates3_x);
                 street_coordinates3_y = Integer.parseInt(street_coordinates3.substring(street_coordinates3.indexOf(",")+1,street_coordinates3.length()-1));
-                //System.out.println(street_coordinates3_y);
             }
 
             Coordinate c1 = new Coordinate(street_coordinates1_x, street_coordinates1_y);
@@ -210,7 +217,6 @@ public class MainWindow extends Application {
             }
             else if (right_angle_street == true)
             {
-                //System.out.println("Three coordinates for street");
                 streets_list.add(Street.defaultStreet(street_id, c1, c2, c3));
             }
 
@@ -260,13 +266,67 @@ public class MainWindow extends Application {
                         System.out.println("Normal street");
                         s.addStop(stop, false);
                         System.out.println(stop.getStreet().getId());
+                        stops_list.add(stop);
 
+                    }
+                    else
+                    {
+                        System.out.println("Right angle street");
+                        s.addStop(stop, true);
+                        System.out.println(stop.getStreet().getId());
+                        stops_list.add(stop);
                     }
 
                 }
             }
         }
 
-        return null;
+        return stops_list;
+    }
+
+    public TransportLine setScheduleLines(ArrayList<Street> streetArrayList, ArrayList<Stop> stopArrayList) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new FileReader("C:/Users/forto/IdeaProjects/proj/lib/transportSchedule.txt"));
+        String line = null;
+
+        line = br.readLine();
+        String line_id = line.substring(line.indexOf("-")+1).trim();
+        System.out.println(line_id);
+        TransportLine transport_line = TransportLine.defaultLine(line_id);
+        System.out.println(transport_line.getId());
+
+        while ((line = br.readLine()) != null) {
+            if (line.contains("Stop"))
+            {
+                for (Stop stop : stopArrayList)
+                {
+                    if (stop.getId().equals(line)) {
+                        transport_line.addStop(stop);
+                    }
+                }
+            }
+            else if (line.contains("Street"))
+            {
+                for (Street s : streetArrayList)
+                {
+                    if (s.getId().equals(line))
+                    {
+                        transport_line.addStreet(s);
+                    }
+                }
+            }
+        }
+
+
+        String res = transport_line.getRoute().stream()
+                .map(entry -> entry.getKey().getId()
+                        + ":"
+                        + entry.getValue()
+                        + ";")
+                .collect(Collectors.joining());
+        System.out.println(res);
+
+        return transport_line;
+
     }
 }
