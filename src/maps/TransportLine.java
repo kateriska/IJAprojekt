df9d2ce881d7125df0a7a697e6fc9c4a18974ca9
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2020-04-02 19:25:03 +0200

    some new methods

--- src/maps/TransportLine.java
+++ src/maps/TransportLine.java
@@ -87,4 +87,133 @@ public class TransportLine {
     }
 
 
+    /*
+    @return arraylist of all coordinates of specified path, they are sorted in the way how vehicle is travelling through them,
+    the right order is important
+     */
+    public ArrayList<Coordinate> transportLinePath()
+    {
+        Coordinate next_street1 = null;
+        Coordinate next_street2 = null;
+        Street next_street = null;
+        ArrayList<Coordinate> line_coordinates = new ArrayList<Coordinate>();
+
+        for (int i = 0; i < getStreetsMap().size(); i++)
+        {
+            Street s = getStreetsMap().get(i);
+            Coordinate this_street1 = s.getCoordinates().get(0);
+            Coordinate this_street2 = s.getCoordinates().get(2);
+
+            if (i + 1 < getStreetsMap().size())
+            {
+                next_street = getStreetsMap().get(i+1);
+                next_street1 = next_street.getCoordinates().get(0);
+                next_street2 = next_street.getCoordinates().get(2);
+            }
+            else
+            {
+                break;
+            }
+
+            for (Stop stop : getStopsMap())
+            {
+                if (stop.getStreet().equals(s))
+                {
+                    line_coordinates.add(stop.getCoordinate());
+                }
+            }
+
+            //11
+            if (this_street1.getX() == next_street1.getX() && this_street1.getY() == next_street1.getY())
+            {
+                line_coordinates.add(this_street1);
+            }
+            //12
+            else if (this_street1.getX() == next_street2.getX() && this_street1.getY() == next_street2.getY())
+            {
+                line_coordinates.add(this_street1);
+            }
+            // 21
+            else if (this_street2.getX() == next_street1.getX() && this_street2.getY() == next_street1.getY())
+            {
+                line_coordinates.add(this_street2);
+            }
+            //22
+            else if (this_street2.getX() == next_street2.getX() && this_street2.getY() == next_street2.getY())
+            {
+                line_coordinates.add(this_street2);
+            }
+
+
+        }
+
+        line_coordinates.add(getStopsMap().get(getStopsMap().size()-1).getCoordinate());
+        return line_coordinates;
+    }
+
+
+    /*
+    @ return arraylist of IDs of Coordinates in the order which is specified path of bus, the correct order is important
+     */
+    public ArrayList<String> transportLinePathIDs()
+    {
+        Coordinate next_street1 = null;
+        Coordinate next_street2 = null;
+        Street next_street = null;
+        ArrayList<String> line_coordinates_ids = new ArrayList<String>();
+
+        for (int i = 0; i < getStreetsMap().size(); i++)
+        {
+            Street s = getStreetsMap().get(i);
+            Coordinate this_street1 = s.getCoordinates().get(0);
+            Coordinate this_street2 = s.getCoordinates().get(2);
+
+            if (i + 1 < getStreetsMap().size())
+            {
+                next_street = getStreetsMap().get(i+1);
+                next_street1 = next_street.getCoordinates().get(0);
+                next_street2 = next_street.getCoordinates().get(2);
+            }
+            else
+            {
+                break;
+            }
+
+            for (Stop stop : getStopsMap())
+            {
+                if (stop.getStreet().equals(s))
+                {
+                    line_coordinates_ids.add(stop.getId());
+                }
+            }
+
+            //11
+            if (this_street1.getX() == next_street1.getX() && this_street1.getY() == next_street1.getY())
+            {
+                line_coordinates_ids.add(s.getId());
+            }
+            //12
+            else if (this_street1.getX() == next_street2.getX() && this_street1.getY() == next_street2.getY())
+            {
+                line_coordinates_ids.add(s.getId());
+            }
+            // 21
+            else if (this_street2.getX() == next_street1.getX() && this_street2.getY() == next_street1.getY())
+            {
+                line_coordinates_ids.add(s.getId());
+            }
+            //22
+            else if (this_street2.getX() == next_street2.getX() && this_street2.getY() == next_street2.getY())
+            {
+                line_coordinates_ids.add(s.getId());
+            }
+
+
+        }
+
+        line_coordinates_ids.add(getStopsMap().get(getStopsMap().size()-1).getId());
+        return line_coordinates_ids;
+    }
+
+
 }
